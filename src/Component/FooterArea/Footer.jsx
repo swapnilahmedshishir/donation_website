bbd8dcf42b1b6784cd1c4cca3980a903e2326662
SWAPNIL AHMED SHISHIR <ahmed66882298@gmail.com>
2024-07-02 16:35:45 +0600

    footer problem solve

--- src/Component/FooterArea/Footer.jsx
+++ src/Component/FooterArea/Footer.jsx
@@ -1,31 +1,38 @@
-import React from 'react';
-
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import'./Footer.CSS';
-import Adress from './FooterPart/Adress';
-import Menu from './FooterPart/Menu';
-import FooterCard from './FooterPart/FooterCard';
-import SocilMedia from './FooterPart/SocailGroup/SocilMedia';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Adress from "./FooterPart/Adress";
+import Menu from "./FooterPart/Menu";
+import FooterCard from "./FooterPart/FooterCard";
+import SocilMedia from "./FooterPart/SocailGroup/SocilMedia";
 const Footer = () => {
   return (
-    <footer className='footer_div'>
-        <Container className='container footer_area'>
-        <Row className='row frist_footer_area'>
-            <Col lg={4} md={6} sm={12} xs={12}><Adress/></Col>
-            <Col lg={3} md={6} sm={12} xs={12}><Menu/></Col>
-            <Col lg={1} md={0} sm={12} xs={12}></Col>
-            <Col lg={4} md={12} sm={12} xs={12}><FooterCard/></Col>
+    <footer className="footer_div">
+      <Container className="container footer_area">
+        <Row className="row frist_footer_area">
+          <Col lg={4} md={6} sm={12} xs={12}>
+            <Adress />
+          </Col>
+          <Col lg={3} md={6} sm={12} xs={12}>
+            <Menu />
+          </Col>
+          <Col lg={1} md={0} sm={12} xs={12}></Col>
+          <Col lg={4} md={12} sm={12} xs={12}>
+            <FooterCard />
+          </Col>
         </Row>
-        <hr/>
-        <Row className='last_footer_area'>
-            <Col lg={6} md={6} sm={12} xs={12} ><h4 className='copyRightText'>Copyright © XYZ Company</h4></Col>
-            <Col lg={6} md={6} sm={12} xs={12}><SocilMedia/> </Col>
+        <hr />
+        <Row className="last_footer_area">
+          <Col lg={6} md={6} sm={12} xs={12}>
+            <h4 className="copyRightText">Copyright © XYZ Company</h4>
+          </Col>
+          <Col lg={6} md={6} sm={12} xs={12}>
+            <SocilMedia />{" "}
+          </Col>
         </Row>
-    </Container>
+      </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
