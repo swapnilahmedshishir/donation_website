835cbe3e3f6806eb79950e3d479435a3f130bcf2
SWAPNIL AHMED SHISHIR <ahmed66882298@gmail.com>
2024-07-02 16:40:54 +0600

    footer problem solve

--- src/Component/BlogPage/Blogpage.jsx
+++ src/Component/BlogPage/Blogpage.jsx
@@ -1,65 +1,83 @@
-import React, { useEffect, useState } from 'react'
-import Articals from './Articals/Articals';
+import { useEffect, useState } from "react";
+import Articals from "./Articals/Articals";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-import './blogpage.css'
-import { Container, Row,Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-
-import fackData from '../../Fackdate/News/donationNews.json';
+import fackData from "../../Fackdate/News/donationNews.json";
 
 const Blogpage = () => {
-
-    const [displayText, setDisplayText] = useState('Realated Topic');
-    const FackData = fackData;
-   
+  const [displayText, setDisplayText] = useState("Realated Topic");
+  const FackData = fackData;
 
   const handleButtonClick = (event) => {
     const buttonText = event.target.textContent;
     setDisplayText(buttonText);
   };
 
-const [articals, setArticals] = useState([]);
-  
+  const [articals, setArticals] = useState([]);
+
   useEffect(() => {
     const matchProduct = FackData.articles.filter(
       (artical) => artical.topic.toLowerCase() === displayText.toLowerCase()
-
     );
     setArticals(matchProduct);
   }, [displayText]);
-  
+
   // console.log(articals);
 
   return (
-    <div className='blog'>
+    <div className="blog">
       <div>
-          <div className="heading_line_top"></div>
-          <h3 className='text-center'>Blog & Resources</h3>
+        <div className="heading_line_top"></div>
+        <h3 className="text-center">Blog & Resources</h3>
       </div>
-        <Container>
-            <Row className='blog_catagoray'>
-                <Col xs={12} md={11} lg={4}><h4>{displayText}</h4></Col>
-                <Col xs={12} md={11} lg={8}>
-                <Link to=''><button className='btn_blog_catagory' onClick={handleButtonClick}>Health care</button></Link>
-                <button className='btn_blog_catagory small' onClick={handleButtonClick}>Humanitarian</button>
-                <button className='btn_blog_catagory small'onClick={handleButtonClick}>Clean water</button>
-                <button className='btn_blog_catagory small' onClick={handleButtonClick}>News</button>
-                </Col>
-            </Row>
-        </Container>
-       <Container>
-       <Row>
-        {
-          articals.map((ar) => {
-           return <Col lg={4} key={ar._id}><div><Articals articals={ar}></Articals></div></Col>
-            
-          })
-        }
-        
-       </Row>
-       </Container>
+      <Container>
+        <Row className="blog_catagoray">
+          <Col xs={12} md={11} lg={4}>
+            <h4>{displayText}</h4>
+          </Col>
+          <Col xs={12} md={11} lg={8}>
+            <Link to="">
+              <button className="btn_blog_catagory" onClick={handleButtonClick}>
+                Health care
+              </button>
+            </Link>
+            <button
+              className="btn_blog_catagory small"
+              onClick={handleButtonClick}
+            >
+              Humanitarian
+            </button>
+            <button
+              className="btn_blog_catagory small"
+              onClick={handleButtonClick}
+            >
+              Clean water
+            </button>
+            <button
+              className="btn_blog_catagory small"
+              onClick={handleButtonClick}
+            >
+              News
+            </button>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          {articals.map((ar) => {
+            return (
+              <Col lg={4} key={ar._id}>
+                <div>
+                  <Articals articals={ar}></Articals>
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Blogpage
+export default Blogpage;
--- src/Component/ContactUsPage/ContactUsPage.jsx
+++ src/Component/ContactUsPage/ContactUsPage.jsx
@@ -1,83 +1,128 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import { FcDonate,FcConferenceCall,FcKindle } from "react-icons/fc";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { FcDonate, FcConferenceCall, FcKindle } from "react-icons/fc";
 import { TfiArrowRight } from "react-icons/tfi";
-import './ContactUsPage.css';
-import { BsFacebook,BsInstagram,BsYoutube,BsWhatsapp } from "react-icons/bs";
-import From from './From/From';
+import { BsFacebook, BsInstagram, BsYoutube, BsWhatsapp } from "react-icons/bs";
+import From from "./From/From";
 
 const ContactUsPage = () => {
   return (
-    <div className='container contact_us_page'>
-        <div className="text-center">
-            <div className="subtitle text-center">Contact us</div>
-            <h1>Get in touch today</h1>
-            <p className="mg-bottom-0"></p>
-        </div>
-    
-    <Row  className="g-4 card_row" >      
-        <Col lg={4} md={4} sm={12} xs={12} className="g-4 card_col text-center" >
-        <div className='card_text_div'>
-            <span className='boostrap_icon_style'><FcDonate/></span>        
-        <h3>Donations</h3>
-        <p>bnq.com <strong><TfiArrowRight/></strong></p>
-        </div>
-        </Col>
-        <Col lg={4} md={4} sm={12} xs={12} className="g-4 card_col text-center" >
-        <div className='card_text_div'>
-            <span className='boostrap_icon_style'><FcConferenceCall/></span>        
-        <h3>Partnerships</h3>
-        <p>info@gmail.com <TfiArrowRight/></p>
-        </div>
-        </Col>  
-        <Col lg={4} md={4} sm={12} xs={12} className="g-4 card_col text-center" >
-        <div className='card_text_div'>
-            <span className='boostrap_icon_style'><FcKindle/></span>        
-        <h3>Press</h3>
-        <p>press@xyz.com <TfiArrowRight/></p>
-        
-        </div>
-        </Col>      
-    </Row>
+    <div className="container contact_us_page">
+      <div className="text-center">
+        <div className="subtitle text-center">Contact us</div>
+        <h1>Get in touch today</h1>
+        <p className="mg-bottom-0"></p>
+      </div>
 
-    <Row>
-        <Col lg={3} md={3} sm={12} xs={12} className='contact_txt'>
-            <h2>Contact us</h2>
-            <div className='contact_left_txt'>
-            XYZ Company Name <br/>
-            USA-nummer: +18143519153 <br/>
-            RSIN: USA2012779 <br/>
-            ANBI-  <br/>
-            </div>
-            <div className='contact_left_txt'>
-            Address: United state <br/>
-            <a href="mailto:info@xyz.gamil.com">Email address: info@xyz.gamil.com</a> <br/>
-            </div>
-            <br/>
-            <div className='contact_left_txt'>
-            Bank account: xyz company<br/>
-            BUNQ - BUNQ 2071 2022<br/>
-            <a href="#">bunq.me</a> 
-            </div>
-            <h3>Follow us on social media</h3>
-            <div className="social-container">
-                <ul className="social-icons">
-                    <li><a href="#"><span><BsFacebook/></span></a></li>
-                    <li><a href="#"><span><BsInstagram/></span></a></li>
-                    <li><a href="#"><span><BsYoutube/></span></a></li>
-                    <li><a href="#"><span><BsWhatsapp/></span></a></li>
-                </ul>
-            </div>
+      <Row className="g-4 card_row">
+        <Col lg={4} md={4} sm={12} xs={12} className="g-4 card_col text-center">
+          <div className="card_text_div">
+            <span className="boostrap_icon_style">
+              <FcDonate />
+            </span>
+            <h3>Donations</h3>
+            <p>
+              bnq.com{" "}
+              <strong>
+                <TfiArrowRight />
+              </strong>
+            </p>
+          </div>
         </Col>
-        <Col lg={9} md={9} sm={{ order: "first" }}  xs={{ order: "first" }} className='from_row'>
-            <From></From>
+        <Col lg={4} md={4} sm={12} xs={12} className="g-4 card_col text-center">
+          <div className="card_text_div">
+            <span className="boostrap_icon_style">
+              <FcConferenceCall />
+            </span>
+            <h3>Partnerships</h3>
+            <p>
+              info@gmail.com <TfiArrowRight />
+            </p>
+          </div>
         </Col>
-    </Row>
-    
+        <Col lg={4} md={4} sm={12} xs={12} className="g-4 card_col text-center">
+          <div className="card_text_div">
+            <span className="boostrap_icon_style">
+              <FcKindle />
+            </span>
+            <h3>Press</h3>
+            <p>
+              press@xyz.com <TfiArrowRight />
+            </p>
+          </div>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col lg={3} md={3} sm={12} xs={12} className="contact_txt">
+          <h2>Contact us</h2>
+          <div className="contact_left_txt">
+            XYZ Company Name <br />
+            USA-nummer: +18143519153 <br />
+            RSIN: USA2012779 <br />
+            ANBI- <br />
+          </div>
+          <div className="contact_left_txt">
+            Address: United state <br />
+            <a href="mailto:info@xyz.gamil.com">
+              Email address: info@xyz.gamil.com
+            </a>{" "}
+            <br />
+          </div>
+          <br />
+          <div className="contact_left_txt">
+            Bank account: xyz company
+            <br />
+            BUNQ - BUNQ 2071 2022
+            <br />
+            <a href="#">bunq.me</a>
+          </div>
+          <h3>Follow us on social media</h3>
+          <div className="social-container">
+            <ul className="social-icons">
+              <li>
+                <a href="#">
+                  <span>
+                    <BsFacebook />
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span>
+                    <BsInstagram />
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span>
+                    <BsYoutube />
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span>
+                    <BsWhatsapp />
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </Col>
+        <Col
+          lg={9}
+          md={9}
+          sm={{ order: "first" }}
+          xs={{ order: "first" }}
+          className="from_row"
+        >
+          <From></From>
+        </Col>
+      </Row>
     </div>
-  )
-}
+  );
+};
 
-export default ContactUsPage
+export default ContactUsPage;
